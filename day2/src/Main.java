13c0a89c9024571694e20325da95be00f1314188
SvenjaG <svenja_galla@web.de>
2023-12-03 20:50:53 +0100

    day 2

---
+++ day2/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("day2/src/input.txt");
+            BufferedReader br = new BufferedReader(fileReader);
+
+            new Part1(br);
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
